de4263d715efcbcd2faecf1bf5cd34f4f58c56b8
Paul Blay <paulmblay@gmail.com>
2016-10-05 15:59:16 +0100

    Not finished

---
+++ configure_boards.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Script is run once and assumes that the target boards are on and ready
+# Default 15 boards if no first arg
+BOARDS=${1:-15}
+# Default to /tmp/script if no second arg
+SCRIPT=${2:-/tmp/script}
+IPADDRPREF="192.168.0."
+REMOTEDIR=${3:-/data}
+REMOTESCRIPT=$REMOTEDIR/##*/$SCRIPT
+for i in `seq 1 $BOARDS`; do
+    scp $SCRIPT $IPADDRPREF$i:$REMOTEDIR
+    echo $SCRIPT
+done
+echo $REMOTESCRIPT
